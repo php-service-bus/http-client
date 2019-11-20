a0ea24455660b40aa59c80b572185fd5e497626d
Stepan Zolotarev <zsl88.logging@gmail.com>
2019-11-20 19:35:50 +0300

    Update dependencies, client fix with amphp/http-client of v4.0.0-rc7 and above

--- src/Artax/functions.php
+++ src/Artax/functions.php
@@ -13,7 +13,7 @@ declare(strict_types = 1);
 namespace ServiceBus\HttpClient\Artax;
 
 use Amp\Dns\NoRecordException;
-use Amp\Http\Client\DnsException;
+use Amp\Dns\DnsException;
 use Amp\Http\Client\ParseException;
 use Amp\Http\Client\Request;
 use Amp\Http\Client\SocketException;
